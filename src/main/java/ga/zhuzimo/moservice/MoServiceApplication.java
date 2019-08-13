ec5bd7218979a67ef1a893a6b035aca5a5d13d46
zhuzhb@10000e.com.cn <zhu7595842zhu>
2019-08-13 15:48:35 +0800

    Initial commit

---
+++ src/main/java/ga/zhuzimo/moservice/MoServiceApplication.java
@@ -0,0 +1,13 @@
+package ga.zhuzimo.moservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoServiceApplication.class, args);
+    }
+
+}
